98f3e15b9211b6ae2078f9d36ab6e6d298b01ec1
RT-Anthony <anthony.bell@roosterteeth.com>
2020-07-29 12:52:07 -0500

    Beginning work on event system, first pass at event types and registering callbacks

--- NodeGraph/NodeEvent.py
+++ NodeGraph/NodeEvent.py
@@ -1,4 +1,21 @@
+from enum import Enum
+
+class EventType(Enum):
+    KEYPRESS   = 0b0000001
+    MOUSECLICK = 0b0000010
+    COPY       = 0b0000100
+    PASTE      = 0b0001000
+    INSERT     = 0b0010000
+    DELETE     = 0b0100000
+
 class NodeEvent(object):
     """description of class"""
+    def __init__(self, action_callback, undo_callback):
+        self._action_callback = action_callback
+        self._undo_callback = undo_callback
 
+    def execute(self, *args, **kwargs):
+        return self._action_callback(args, kwargs)
 
+    def undo(self, *args, **kwargs):
+        return self._undo_callback(args, kwargs)
